3ac87ad2d68895335728863cf5bb3cdbb50dcde7
Caio Alexandre Troti Caetano <caio.caetano@dock.tech>
2020-01-02 09:55:50 -0300

    Remove not used batchs

--- compile2.bat
+++
@@ -1,4 +0,0 @@
-nasm -f elf32 boot/boot.asm -o boot/boot.o
-gcc -m32 -c kernel/kernel.c -o kernel/kernel.o -ffreestanding -nostdlib -nostdinc
-ld -T linker.ld -o kernel.bin boot/boot.o drivers/screen.o drivers/ports.o kernel/kernel.o -build-id=none
-objcopy -O elf32-i386 kernel.bin kernel.elf
